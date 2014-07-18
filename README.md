2b45e7c66a3fa43d800bc56256f433c9331fab0f
Chris Smeele <cjsmeele@xs4all.nl>
2014-07-18 13:43:10 +0200

    Added README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+NAME
+====
+
+HADDOCK-WebUI - Web interface to HADDOCK
+
+DESCRIPTION
+===========
+
+HADDOCK-WebUI is a Python2/Flask application that serves as a frontend to
+[HADDOCK](http://www.nmr.chem.uu.nl/haddock/).
+
+More documentation to be added later.
+
+SEE ALSO
+========
+
+- [CNSParser](https://github.com/csmeele/CNSParser)
--- config.py
+++ config.py
@@ -3,11 +3,6 @@ class Config(object):
     # a dropdown is rendered instead of a radio input.
     FRONTEND_RADIO_MAX  = 3
 
-    # A list of CSS stylesheets which will be loaded in order
-    FRONTEND_STYLESHEETS = [
-        '/static/css/frontend.css'
-    ]
-
     FRONTEND_ACCESSLEVEL_FILE = 'res/accesslevels.json'
     FRONTEND_MODEL_FILE       = 'res/model.json'
 
