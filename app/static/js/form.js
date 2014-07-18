518ae77cb6d6548dfd91e36d9de932b7e6e2c0b1
Chris Smeele <cjsmeele@xs4all.nl>
2014-07-18 16:47:00 +0200

    Added support for form section folding

--- app/static/js/form.js
+++ app/static/js/form.js
@@ -9,24 +9,44 @@ $(function(){
 
 		for(var i=0; i<formLevels.length; i++){
 			if(i <= formLevel){
-				$("form .level-"+formLevels[i]).removeClass('disabled');
-				$("form .level-"+formLevels[i]+" input").prop('disabled', false);
+				$('form .level-'+formLevels[i]).removeClass('disabled');
+				$('form .level-'+formLevels[i]+" input").prop('disabled', false);
 
 				if(Config.hideDisabledComponents)
-					$("form .level-"+formLevels[i]).slideDown(120);
+					$('form .level-'+formLevels[i]).slideDown(120);
 			}else{
-				$("form .level-"+formLevels[i]).addClass('disabled');
-				$("form .level-"+formLevels[i]+" input").prop('disabled', true);
+				$('form .level-'+formLevels[i]).addClass('disabled');
+				$('form .level-'+formLevels[i]+" input").prop('disabled', true);
 
 				if(Config.hideDisabledComponents)
-					$("form .level-"+formLevels[i]).slideUp(120);
+					$('form .level-'+formLevels[i]).slideUp(120);
 			}
+		}
+	}
+
+	function toggleSection(section){
+		var toggleButton = $(section).find('header > .togglebutton')[0];
+
+		if($(section).hasClass('folded')){
+			$(toggleButton).removeClass('fa-angle-double-up');
+			$(toggleButton).addClass('fa-angle-double-down');
+			$(section).removeClass('folded');
+			$($(section).find('.content')[0]).slideDown(120);
+		}else{
+			$(toggleButton).removeClass('fa-angle-double-down');
+			$(toggleButton).addClass('fa-angle-double-up');
+			$(section).addClass('folded');
+			$($(section).find('.content')[0]).slideUp(120);
 		}
 	}
 
 	$('.levelchooser li').click(function(e){
 		setLevel($(this).data('name'));
 		e.preventDefault();
+	});
+
+	$('form section > header').click(function(e){
+		toggleSection($(this).parent('section')[0]);
 	});
 
 	setLevel(formLevel);
