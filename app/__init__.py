5f335108a6e72e508e3ebdbbc2c0b8bf209ede7e
Chris Smeele <cjsmeele@xs4all.nl>
2014-07-22 10:41:07 +0200

    Implemented model and accesslevel caching

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
-
 # Initialize application
 app = Flask(__name__)
 
 # Load config
 from config import config
 app.config.from_object(config['default'])
+
+if app.config['CACHE_HTML'] or app.config['CACHE_MODEL'] or app.config['CACHE_ACCESSLEVELS']:
+    from flask.ext.cache import Cache
+    cache = Cache(app,config={'CACHE_TYPE': app.config['CACHE_BACKEND']})
+    cache.init_app(app)
+else:
+    cache = None
 
 # Set up logger
 import logging
--- config.py
+++ config.py
@@ -6,6 +6,19 @@ class Config(object):
     FRONTEND_ACCESSLEVEL_FILE = 'res/accesslevels.json'
     FRONTEND_MODEL_FILE       = 'res/model.json'
 
+    # Turn these off if there is no cache backend available or Flask-Cache is
+    # not installed
+    CACHE_HTML  = True
+    CACHE_MODEL = True
+    CACHE_ACCESSLEVELS = CACHE_MODEL
+
+    # The simple backend should always be supported when Flask-Cache is installed.
+    # See http://pythonhosted.org/Flask-Cache/#configuring-flask-cache for other
+    # options.
+    # Note that some cache backends may need additional configuration parameters
+    # which can't be passed through this config object (yet).
+    CACHE_BACKEND = 'simple'
+
 class ConfigDefault(Config):
     pass
 
